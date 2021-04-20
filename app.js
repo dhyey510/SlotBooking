4049a43324d32b56df36c596031b832ebe4e67d3
dhyey510 <dhyey870@gmail.com>
2021-04-20 17:38:57 +0530

    final

--- app.js
+++ app.js
@@ -1,54 +1,208 @@
 const express=require('express');
 const app=express();
-
-var bodyParser=require('body-parser');
+const path=require('path');
+const session=require("express-session");
 const mongoose=require('mongoose');
+const passport=require('passport');
+const localStrategy=require("passport-local");
+const passportLocalMongoose=require('passport-local-mongoose');
+const bodyParser=require('body-parser');
+const multer=require('multer');
 
-var userSchema=new mongoose.Schema({
-    name: String,
-    app_time: String,
-    max_time:Number,
-    imgurl:String
-});
-
-var salonSchema=new mongoose.Schema({
-    name: String,
-    address: String,
-    website: String,
-    img: String,
-    slotusers:[userSchema]
-});
-var salon= mongoose.model("Salon", salonSchema);
-
+// passport config
+app.use(session({
+    secret:"Dhyey website",
+    resave:false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
+
+var slotSchema=new mongoose.Schema({
+    username: String,
+    imgurl:String,
+    salonname:String,
+    app_time: String,
+    max_time:Number,
+});
+
+var userSchema=new mongoose.Schema({
+    username: String,
+    email:String,
+    password:String,
+    issalon:String,
+    imgurl:String,
+    address:String,
+    website:String,
+    slot:[slotSchema]
+});
+userSchema.plugin(passportLocalMongoose);
+
+var User= mongoose.model("User", userSchema);
+var slot=mongoose.model('slot',slotSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// multer
+const storage=multer.diskStorage({
+    destination:'./public/uploadas',
+    filename : function(req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }   
+});
+
+const upload=multer({
+    storage:storage
+}).single('img-uploade');
 
 
 app.get('/',function(req,res){
     res.render('index.ejs');
 });
 
-app.get('/login',function(req,res){
-    res.render('login.ejs');
+app.get('/about',function(req,res){
+    res.render('about.ejs');
 });
 
-app.get('/book',function(req,res){
-    salon.find({},function(err,salons){
+app.post('/feedback',function(req,res){
+    console.log(`name : ${req.body.username}`);
+    console.log(`email : ${req.body.email}`);
+    console.log(`feedback : ${req.body.add}`);
+    res.redirect('/');
+});
+
+app.get('/login',function(req,res){
+    if(req.isAuthenticated()){
+        if(req.user.issalon=="Salon"){
+            res.redirect(`/${req.user.id}/editsalon`);
+        }else{
+            res.redirect(`/${req.user.id}/book`);
+        }
+    }else{
+        res.render('login.ejs');
+    }
+});
+
+app.post('/login',(req, res, next) => {
+    passport.authenticate('local',
+    (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+  
+      if (!user) {
+        return res.redirect('/login');
+      }
+  
+      req.logIn(user, function(err) {
+        if (err) {
+          return next(err);
+        }
+        if(user.issalon=="Salon"){
+            return res.redirect(`/${user.id}/editsalon`);
+        }else{
+            return res.redirect(`/${user.id}/book`);
+        }
+      });
+    })(req, res, next);
+ });
+
+app.post('/signup',function(req,res){
+    upload(req,res,(err)=>{
+        if(err){
+            console.log(err);
+            res.render('login.ejs');
+        }else{
+            let newUser=new User({
+                email:req.body.email,
+                imgurl:req.file.filename,
+                username:req.body.username,
+                issalon:req.body.issalon,
+            });
+            User.register(newUser,req.body.password,function(err,user){
+                if(err){
+                    console.log(err);
+                    res.redirect('/login');
+                }else{
+                    passport.authenticate("local")(req,res,function(){
+                        if(req.body.issalon == "Salon"){
+                            res.redirect(`/${req.user.id}/editsalon`);
+                        }else{
+                            res.redirect(`/${req.user.id}/book`);
+                        }
+                    });    
+                }
+            });
+        }
+    });
+});
+
+app.get('/:id/editsalon',function(req,res){
+    res.render('editsalon.ejs',{user:req.user});
+});
+
+app.post('/:id/savepro',function(req,res){
+    let newUser={
+        username:req.body.username,
+        email:req.body.email,
+        website:req.body.web,
+        address:req.body.add
+    };
+    User.findByIdAndUpdate(req.params.id,newUser,function(err,user){
         if(err){
             console.log(err);
         }else{
-            res.render('Book.ejs',{saloninfo:salons});
+            res.redirect(`/${req.user.id}/salonhome`);
+        }
+    });
+});
+
+app.get('/:id/salonhome',function(req,res){
+    res.render('salonhome.ejs',{user:req.user});
+});
+
+app.get('/:id/:slotid/cnf',function(req,res){
+    User.findById(req.params.id,function(err,user){
+        for(let i=0;i<user.slot.length;i++){
+            if(user.slot[i].id==req.params.slotid){
+                user.slot.splice(i,1);
+                user.save(function(err,us){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        res.redirect(`/${req.user.id}/salonhome`);
+                    }
+                });
+                break;
+            }
+        }
+    });
+});
+
+
+
+app.get('/:id/book',function(req,res){
+    User.find({issalon:"Salon"},function(err,salons){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('Book.ejs',{saloninfo:salons,user:req.user});
         }
     });
 });
 
 app.get('/book/:name',function(req,res){
     let salonname=req.params.name;
-    salon.find({name:salonname},function(err,findsalon){
+    User.find({username:salonname,issalon:"Salon"},function(err,findsalon){
         if(err){
             console.log(err);
         }else{
+            console.log(findsalon);
             res.render('slot.ejs',{info:findsalon});
         }
     });
@@ -67,16 +221,16 @@ app.post('/book/:name',function(req,res){
         maximum_time=maximum_time+1;
     }
     const new_req={
-                     name:req.body.usname,
+                     username:req.body.usname,
                      app_time:req.body.aptime,
                      max_time:maximum_time,
-                     imgurl:"/Img/unnamed.jpg"
+                     imgurl:req.user.imgurl
                  };
-    salon.find({name:salonname},function(err,findsalon){
+    User.find({username:salonname,issalon:"Salon"},function(err,findsalon){
         if(err){
                 console.log(err);
         }else{
-                findsalon[0].slotusers.push(new_req);
+                findsalon[0].slot.push(new_req);
                 findsalon[0].save(function(err,sa){
                     if(err){
                         console.log(err);
@@ -87,12 +241,21 @@ app.post('/book/:name',function(req,res){
     });    
 });
 
+app.get('/logout',function(req,res){
+    req.logout();
+    res.redirect('/');
+});
+
+
+
+
+
 mongoose.connect('mongodb+srv://dhyey:dhyey510@cluster0-1954n.mongodb.net/test?retryWrites=true&w=majority',
         {useNewUrlParser: true,useUnifiedTopology: true},
         ()=>{
     console.log(`mongoose connect `);
 });
-
+mongoose.set('useCreateIndex', true);
 
 app.listen(3000,function(){
     console.log(`listening on 3000 : `);
--- public/Js/form.js
+++ public/Js/form.js
@@ -1,51 +1,32 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCAMdAakqbkODxLzTynFje48YM6qW-qvRs",
-    authDomain: "web-practice-370f5.firebaseapp.com",
-    databaseURL: "https://web-practice-370f5.firebaseio.com",
-    projectId: "web-practice-370f5",
-    storageBucket: "web-practice-370f5.appspot.com",
-    messagingSenderId: "644869696650",
-    appId: "1:644869696650:web:8f92788000bab37af99173",
-    measurementId: "G-7L0J66Z9FJ"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  const auth=firebase.auth();
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
 
-  function signup(){
-      var email = document.getElementById("email");
-      var password = document.getElementById("password");
-      const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-      promise.catch(e => alert(e.mesaage));
-      alert("signed up successful");
-      window.location="../Book.ejs"
-  }
-  function signin(){
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.mesaage));
-    //alert("signed IN"+ email.value); 
-    window.location="../../views/Book.ejs"
-  }
-  function signout(){
-       auth.signOut();
-      //  alert("sign out successful");
-  }
-  auth.onAuthStateChanged(function(user)
-  {
-      if(user){
-          var email =user.email;
-          // window.location="../Book.html"
-      }else{
-        alert("hello");
-      }
+// img upload
+const fileupld=document.querySelector('#profile-img');
+const filebtn=document.querySelector('#img-upload');
 
-  });
+filebtn.addEventListener("change",function(){
+    const file=this.files[0];
 
+    if(file){
+        const reader=new FileReader();
 
-  
+        reader.addEventListener("load",function(){
+            fileupld.setAttribute("src",this.result);
+        });
+
+        reader.readAsDataURL(file);
+
+    }else{
+
+    }
+});
